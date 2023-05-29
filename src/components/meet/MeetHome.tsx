ba4c6d59942b2f58fdfe035657d0bcf4ad8e33ad
PauloScotti <paulo.scotti@hotmail.com>
2023-05-29 18:47:43 -0300

    Aula 03 parte 03

---
+++ src/components/meet/MeetHome.tsx
@@ -0,0 +1,10 @@
+
+export const MeetHome = () => {
+    return (
+        <div className="container-principal">
+            <div className="container-meet">
+
+            </div>
+        </div>
+    );
+}
