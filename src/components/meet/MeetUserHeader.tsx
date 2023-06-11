fbb41dd00256f42d58a4c2db28404664607c3a40
PauloScotti <paulo.scotti@hotmail.com>
2023-06-11 15:30:51 -0300

    Aula 07 parte 02

--- src/components/meet/MeetUserHeader.tsx
+++ src/components/meet/MeetUserHeader.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import addIcon from '../../assets/images/add.svg';
+import React from 'react';
 
+type MeetUserHeaderProps = {
+    isLink?: boolean
+}
 
-export const MeetUserHeader = () => {
+export const MeetUserHeader: React.FC<MeetUserHeaderProps> = ({ isLink }) => {
 
     const navigate = useNavigate();
     const mobile = window.innerWidth <= 992;
@@ -14,7 +18,7 @@ export const MeetUserHeader = () => {
 
     return (
         <div className="container-user-header">
-            <span>Minhas reuniões</span>
+            <span>{isLink ? 'Reunião' : 'Minhas reuniões'}</span>
             <div>
                 <p>Olá, {name}</p>
                 {!mobile && <img src={addIcon} alt='Adicionar reunião' onClick={navigateToAdd} />}
